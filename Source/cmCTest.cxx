57b2e33c7ab7f4f316accc2d65d37ded61367596
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-05-04 14:23:24 -0400

    cmCTest: Fix typo

--- Source/cmCTest.cxx
+++ Source/cmCTest.cxx
@@ -2540,7 +2540,7 @@ int cmCTest::Run(std::vector<std::string> &args, std::string* output)
       }
     }
 
-  // now what sould cmake do? if --build-and-test was specified then
+  // now what should cmake do? if --build-and-test was specified then
   // we run the build and test handler and return
   if(cmakeAndTest)
     {
